7b50acaa6b7a1cfab1ff560319a187f01d7515c3
origin <giorgiogmq@gmail.com>
2023-09-22 13:16:11 -0300

    Criado entidade

---
+++ src/main/java/com/estudo/myfoodapi/domain/entity/Restaurante.java
@@ -0,0 +1,20 @@
+package com.estudo.myfoodapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+}
