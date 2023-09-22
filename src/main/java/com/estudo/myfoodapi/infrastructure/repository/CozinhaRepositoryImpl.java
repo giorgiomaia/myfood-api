a90bc7d8f8884861265bdf583c70734b932e15ed
origin <giorgiogmq@gmail.com>
2023-09-22 12:37:47 -0300

    implementado repositorio

---
+++ src/main/java/com/estudo/myfoodapi/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.estudo.myfoodapi.infrastructure.repository;
+
+import com.estudo.myfoodapi.domain.entity.Cozinha;
+import com.estudo.myfoodapi.domain.repository.CozinhaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Cozinha> todas() {
+        return entityManager.createQuery("FROM Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        return entityManager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+        return entityManager.merge(cozinha);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+        entityManager.remove(cozinha);
+    }
+}
