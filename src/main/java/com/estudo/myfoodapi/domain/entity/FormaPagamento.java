d086b091c0cf5b5967d48c3a602a308c322f0e2a
origin <giorgiogmq@gmail.com>
2023-09-26 15:28:44 -0300

    Criado entidade

---
+++ src/main/java/com/estudo/myfoodapi/domain/entity/FormaPagamento.java
@@ -0,0 +1,19 @@
+package com.estudo.myfoodapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FormaPagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+}
