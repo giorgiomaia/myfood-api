0fa286562449d8a4a2c305e49d2d2fc6dc40f71b
origin <giorgiogmq@gmail.com>
2023-09-27 13:10:33 -0300

    Criado implementação do repository

---
+++ src/main/java/com/estudo/myfoodapi/infrastructure/repository/CidadeRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.estudo.myfoodapi.infrastructure.repository;
+
+import com.estudo.myfoodapi.domain.entity.Cidade;
+import com.estudo.myfoodapi.domain.repository.CidadeRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CidadeRepositoryImpl implements CidadeRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Cidade> listar() {
+        return entityManager.createQuery("FROM Cidade", Cidade.class).getResultList();
+    }
+
+    @Override
+    public Cidade buscar(Long id) {
+        return entityManager.find(Cidade.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Cidade salvar(Cidade cidade) {
+        return entityManager.merge(cidade);
+    }
+
+    @Override
+    @Transactional
+    public void remover(Cidade cidade) {
+        cidade = buscar(cidade.getId());
+        entityManager.remove(cidade);
+    }
+}
