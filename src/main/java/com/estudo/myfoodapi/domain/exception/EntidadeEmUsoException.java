ecbd747d6c439832d9c7ebe84b3f5d12b960038f
origin <giorgiogmq@gmail.com>
2023-11-14 09:35:00 -0300

    Criando exceções personalizadas

---
+++ src/main/java/com/estudo/myfoodapi/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,13 @@
+package com.estudo.myfoodapi.domain.exception;
+
+import java.io.Serial;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeEmUsoException(String message) {
+        super(message);
+    }
+}
