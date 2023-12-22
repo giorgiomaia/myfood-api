5a811454f7ac13da1bdb60d8afe75d31243acb8e
origin <giorgiogmq@gmail.com>
2023-12-22 18:15:31 -0300

    Incluindo métodos de buscar, atualizar e remover no controller

--- src/main/java/com/estudo/myfoodapi/api/controller/EstadoController.java
+++ src/main/java/com/estudo/myfoodapi/api/controller/EstadoController.java
@@ -1,12 +1,17 @@
 package com.estudo.myfoodapi.api.controller;
 
 import com.estudo.myfoodapi.domain.entity.Estado;
+import com.estudo.myfoodapi.domain.exception.EntidadeEmUsoException;
+import com.estudo.myfoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.estudo.myfoodapi.domain.service.EstadoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/estados")
@@ -24,10 +29,44 @@ public class EstadoController {
         return estadoService.listar();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long id) {
+        Estado estado = estadoService.buscar(id);
+        if (Objects.nonNull(estado)) {
+            return ResponseEntity.ok(estado);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @PostMapping
     public ResponseEntity<Estado> adicionar(@RequestBody Estado estado) {
         estado = estadoService.salvar(estado);
         return ResponseEntity.ok(estado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estado> atualizar(@PathVariable Long id, @RequestBody Estado estado) {
+        Estado atualizaEstado = estadoService.buscar(id);
+        if (Objects.nonNull(atualizaEstado)) {
+            BeanUtils.copyProperties(estado, atualizaEstado, "id");
+            atualizaEstado = estadoService.salvar(atualizaEstado);
+            return ResponseEntity.ok(atualizaEstado);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        try {
+            estadoService.remover(id);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
