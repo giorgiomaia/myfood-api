11e0ae952dbae965b550c4772bc528363f5157b0
origin <giorgiogmq@gmail.com>
2023-12-22 18:11:16 -0300

    Ajustado repositório

--- src/main/java/com/estudo/myfoodapi/domain/repository/EstadoRepository.java
+++ src/main/java/com/estudo/myfoodapi/domain/repository/EstadoRepository.java
@@ -6,10 +6,7 @@ import java.util.List;
 
 public interface EstadoRepository {
     List<Estado> listar();
-
     Estado buscar(Long id);
-
     Estado salvar(Estado estado);
-
-    void remover(Estado estado);
+    void remover(Long id);
 }
