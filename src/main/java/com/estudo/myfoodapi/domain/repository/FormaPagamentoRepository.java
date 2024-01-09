e4c7cfa0874640e4ca390c06e2732d07d88e633b
origin <giorgiogmq@gmail.com>
2024-01-08 21:25:25 -0300

    Refatorado repository para usar springDataJpa

--- src/main/java/com/estudo/myfoodapi/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/estudo/myfoodapi/domain/repository/FormaPagamentoRepository.java
@@ -1,15 +1,10 @@
 package com.estudo.myfoodapi.domain.repository;
 
 import com.estudo.myfoodapi.domain.entity.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+@Repository
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
-public interface FormaPagamentoRepository {
-    List<FormaPagamento> listar();
-
-    FormaPagamento buscar(Long id);
-
-    FormaPagamento salvar(FormaPagamento formaPagamento);
-
-    void remover(FormaPagamento formaPagamento);
 }
