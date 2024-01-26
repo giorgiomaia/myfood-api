e116e1604a89bae158d5df23ba120a131cdadf93
origin <giorgiogmq@gmail.com>
2024-01-26 09:00:34 -0300

    Criado interface de repository personalizado

---
+++ src/main/java/com/estudo/myfoodapi/domain/repository/RestauranteRepositoryPersonalizado.java
@@ -0,0 +1,10 @@
+package com.estudo.myfoodapi.domain.repository;
+
+import com.estudo.myfoodapi.domain.entity.Restaurante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestauranteRepositoryPersonalizado {
+    List<Restaurante> buscarPorFiltro(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal);
+}
