8a9864e669e411476c8c947c62ffdbf4335f301c
Giorgio Maia <giorgiogmq@gmail.com>
2024-04-15 10:04:44 -0300

    Incluido espeficication no repositorio

--- src/main/java/com/estudo/myfoodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/com/estudo/myfoodapi/domain/repository/RestauranteRepository.java
@@ -2,9 +2,11 @@ package com.estudo.myfoodapi.domain.repository;
 
 import com.estudo.myfoodapi.domain.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository
-        extends JpaRepository<Restaurante, Long>, RestauranteRepositoryPersonalizado {
+        extends JpaRepository<Restaurante, Long>, RestauranteRepositoryPersonalizado,
+                JpaSpecificationExecutor<Restaurante> {
 }
