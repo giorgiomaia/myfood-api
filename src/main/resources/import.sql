3055d9fa2b39dc707723765a87f497f2249cf559
Giorgio Maia <giorgiogmq@gmail.com>
2024-04-16 10:06:19 -0300

    Ajustado import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,3 @@
--- Cozinhas
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
-
--- Restaurantes
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
-
 -- Estados
 insert into estado (nome) values ('Minas Gerais');
 insert into estado (nome) values ('São Paulo');
@@ -18,6 +9,15 @@ insert into cidade (nome, estado_id) values ('Belo Horizonte', 1);
 insert into cidade (nome, estado_id) values ('São Paulo', 2);
 insert into cidade (nome, estado_id) values ('Campinas', 2);
 insert into cidade (nome, estado_id) values ('Fortaleza', 3);
+
+-- Cozinhas
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Indiana');
+
+-- Restaurantes
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values ('Thai Gourmet', 10, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Thai Delivery', 9.50, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Tuk Tuk Comida Indiana', 15, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- Formas de Pagamento
 insert into forma_pagamento (descricao) values ('Cartão de crédito');
